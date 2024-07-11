70ea6bb83a97b66be6f2eeb2fc779d204a632f24
Lucas Hackbart Döhnert <lucashackd@gmail.com>
2024-07-11 14:58:56 -0300

    add: firebase signup

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,15 +1,60 @@
 import React, {useState} from 'react';
 import {Body, TextInput} from './styles';
 import MyButton from '../../components/MyButton';
+import auth from '@react-native-firebase/auth';
+import {Alert} from 'react-native';
+import {CommonActions} from '@react-navigation/native';
 
-const SignUp = () => {
+const SignUp = ({navigation}) => {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   const [confirmPass, setConfirmPass] = useState('');
 
   const cadastrar = () => {
-    alert('foi');
+    if (nome !== '' && email !== '' && pass !== '' && confirmPass !== '') {
+      auth()
+        .createUserWithEmailAndPassword(email, pass)
+        .then(() => {
+          Alert.alert('Informação', 'Usuário cadastrado com sucesso.');
+          navigation.dispatch(
+            CommonActions.reset({
+              index: 0,
+              routes: [{name: 'Home'}],
+            }),
+          );
+        })
+        .catch(e => {
+          console.log('SignIn: erro em entrar ' + e);
+          switch (e.code) {
+            // case 'auth/invalid-credential':
+            //   Alert.alert('ERRO', 'Credencial inválida.');
+            //   break;
+            case 'auth/email-already-in-use':
+              Alert.alert('ERRO', 'Email já está em uso.');
+              break;
+            case 'auth/invalid-email':
+              Alert.alert('ERRO', 'Email inválido.');
+              break;
+            case 'auth/operation-not-allowed':
+              Alert.alert('ERRO', 'Problemas ao cadastrar o usuário.');
+              break;
+            case 'auth/weak-password':
+              Alert.alert(
+                'ERRO',
+                'Senha fraca, por favor, digite uma senha forte.',
+              );
+              break;
+            default:
+              Alert.alert('ERRO AO LOGAR', `${e}`);
+          }
+        });
+    } else {
+      Alert.alert(
+        'ATENÇÃO',
+        'Por favor, preencha os campos EMAIL e SENHA para entrar.',
+      );
+    }
   };
   return (
     <Body>
